37bb74982b359b403d3571c9533a0bf19455fd3a
Rachana Shrestha <237523046+rachanastha700-lang@users.noreply.github.com>
2026-02-05 01:29:18 +0000

    EtchASketch demo-2

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,35 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".btn");
+const color = document.querySelector(".color");
+const clear = document.querySelector(".clear");
+const eraser = document.querySelector(".erase");
+const select = document.querySelector("select");
+let currenttool = "rainbow";
 //every time click here
+let isdragging = false;
+
+document.addEventListener("mousedown",()=>{
+isdragging = true;
+});
+document.addEventListener("mouseup",()=>{
+isdragging = false;
+});
+
+
+color.addEventListener("click",()=>{
+    currenttool = "rainbow";
+    });
+select.addEventListener("change",function(e){
+    currenttool = e.target.value;
+});  
+eraser.addEventListener("click",()=>{
+    currenttool = "erase";
+    });
+clear.addEventListener("click",()=>{
+        container.innerHTML ="";
+    });
+
+//the grid button --
 button.addEventListener("click",()=>{
 
 let n = document.createElement("input");
@@ -21,15 +50,51 @@ div.style.height = `${100/n}%`;
 
 //for color down
  let num = 0.1;
+ //rainbow color button
+ 
 div.addEventListener("mouseover",function(e){
-    
-   e.target.style.backgroundColor = getrandomcolor();
-    
+    if(!isdragging)return ; //if true-->false then returns not going down
+   if(currenttool === "rainbow"){
+e.target.style.backgroundColor = getrandomcolor();
+   let brd =e.target.style.backgroundColor;
+    e.target.style.border =`0.5px solid ${brd}`;
         e.target.style.opacity = `${num}`;
         num = num+0.1;
-
+   }
+   else if(currenttool === "CBP"){
+    e.target.style.backgroundColor = "#F7ADC3";
+    let pink = e.target.style.backgroundColor;
+    e.target.style.border =`0.5px solid ${pink}`;
+    e.target.style.opacity = 1;
+   }
+   else if(currenttool === "FT"){
+    e.target.style.backgroundColor = "#FCC5D9";
+    let pink = e.target.style.backgroundColor;
+    e.target.style.border =`0.5px solid ${pink}`;
+    e.target.style.opacity = 1;
+   }
+   else if (currenttool === "MM"){
+    e.target.style.backgroundColor = "#FADDE3";
+    let pink = e.target.style.backgroundColor;
+    e.target.style.border =`0.5px solid ${pink}`;
+    e.target.style.opacity = 1;
+   }
+   else if(currenttool === "FW"){
+    e.target.style.backgroundColor = "#F7F5ED";
+    let pink = e.target.style.backgroundColor;
+    e.target.style.border =`0.5px solid ${pink}`;
+    e.target.style.opacity = 1;
+   }
+   else if(currenttool === "erase")
+   {
+    e.target.style.backgroundColor = "#ffffff";
+    e.target.style.border = `0.5px solid #ffffff`;
+   }
+   
     });
    
+  
+
 }
 }//loop end
 
